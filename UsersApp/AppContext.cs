bb1d44b0f46a25f3785cc420516aaf4c45961b05
KrutoiProgram <andrey23451dss2@gmail.com>
2022-10-14 23:18:02 +0500

    Ошибка с бд

---
+++ UsersApp/AppContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersApp
+{
+    class AppContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppContext() : base("DefaultConnection") { }
+
+    }
+}
---
+++ UsersApp/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UsersApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AppContext db;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            db = new AppContext();
+
+            if (db != null)
+            {
+                List<User> users = db.Users.ToList();
+                string str = "";
+                foreach (User user in users) { str += $"Login: {user.Login} | "; }
+            }
+        }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string pass = passBox.Password.Trim();
+            string pass_2 = PassBox_2.Password.Trim();
+            string email = textBoxEmail.Text.Trim().ToLower();
+
+            if (login.Length < 5)
+            {
+                textBoxLogin.ToolTip = "Это поле введено не корректно!";
+                textBoxLogin.Background = Brushes.DarkRed;
+            }
+            else if (pass.Length < 5)
+            {
+                passBox.ToolTip = "Это поле введено не корректно!";
+                passBox.Background = Brushes.DarkRed;
+            }
+            else if (pass != pass_2)
+            {
+                passBox.ToolTip = "Это поле введено не корректно!";
+                passBox.Background = Brushes.DarkRed;
+            }
+            else if (email.Length < 5 || !email.Contains("@") || !email.Contains("."))
+            {
+                textBoxEmail.ToolTip = "Это поле введено не корректно!";
+                textBoxEmail.Background = Brushes.DarkRed;
+            }
+            else
+            {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+                passBox.ToolTip = "";
+                passBox.Background = Brushes.Transparent;
+                PassBox_2.ToolTip = "";
+                PassBox_2.Background = Brushes.Transparent;
+                textBoxEmail.ToolTip = "";
+                textBoxEmail.Background = Brushes.Transparent;
+
+                MessageBox.Show("Всё хорошо!");
+                User user = new User(login, email, pass);
+
+
+                db.Users.Add(user);
+                db.SaveChanges();
+            }
+        }
+    }
+}
